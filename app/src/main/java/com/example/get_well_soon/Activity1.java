0e67a7fb29259c3f8bba39f9b9849b6117be5801
srijan-bytes <srijansaxena18@gmail.com>
2020-05-12 23:21:21 +0530

    Created activities 2 and 5

--- app/src/main/java/com/example/get_well_soon/Activity1.java
+++ app/src/main/java/com/example/get_well_soon/Activity1.java
@@ -2,7 +2,11 @@ package com.example.get_well_soon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.get_well_soon.ui.login.LoginActivity;
 
 public class Activity1 extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class Activity1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_1);
+    }
+    public void sign(View view)
+    {
+        Intent i=new Intent(this, LoginActivity.class);
+        startActivity(i);
+    }
+    public void register(View view)
+    {
+        Intent i=new Intent(this, Activity2.class);
+        startActivity(i);
+    }
+    public void back(View view)
+    {
+        Intent i=new Intent(this, MainActivity.class);
+        startActivity(i);
     }
 }
