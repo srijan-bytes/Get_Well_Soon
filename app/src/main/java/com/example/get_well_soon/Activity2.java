56f26bb2d10b0c9ca433904aaae413bee9987859
srijan-bytes <srijansaxena18@gmail.com>
2020-05-15 00:29:46 +0530

    Added activity2_1 and improved Activity2

--- app/src/main/java/com/example/get_well_soon/Activity2.java
+++ app/src/main/java/com/example/get_well_soon/Activity2.java
@@ -2,7 +2,9 @@ package com.example.get_well_soon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Activity2 extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class Activity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+    }
+    private void next(View view)
+    {
+        Intent i=new Intent(this, Activity2_1.class);
+        startActivity(i);
+    }
+
+    private void previous(View view)
+    {
+        Intent i=new Intent(this, Activity1.class);
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/com/example/get_well_soon/Activity2_1.java
@@ -0,0 +1,13 @@
+package com.example.get_well_soon;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity2_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity2_1);
+    }
+}
