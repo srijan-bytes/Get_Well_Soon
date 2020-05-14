397f00ab34561e5a3ac23e58a7c74bc92d4f42bb
srijan-bytes <srijansaxena18@gmail.com>
2020-05-15 01:03:38 +0530

    Added activity7_1

--- app/src/main/java/com/example/get_well_soon/Activity7_1.java
+++ app/src/main/java/com/example/get_well_soon/Activity7_1.java
@@ -6,22 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class Activity7 extends AppCompatActivity {
+public class Activity7_1 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_7);
+        setContentView(R.layout.activity_activity7_1);
     }
     private void previous(View view)
     {
-        Intent i=new Intent(this, MainActivity.class);
-        startActivity(i);
-    }
-
-    private void next(View view)
-    {
-        Intent i=new Intent();
+        Intent i=new Intent(this, Activity7.class);
         startActivity(i);
     }
 }
