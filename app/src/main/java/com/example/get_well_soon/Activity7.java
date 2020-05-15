168830b86773955f9f8ccc5615ae4da2d2ed00f5
srijan-bytes <srijansaxena18@gmail.com>
2020-05-15 05:40:44 +0530

    Added icon

--- app/src/main/java/com/example/get_well_soon/Activity7.java
+++ app/src/main/java/com/example/get_well_soon/Activity7.java
@@ -5,9 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class Activity7 extends AppCompatActivity {
+public class Activity7 extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    Spinner spinner;
     Button previous,next;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +32,11 @@ public class Activity7 extends AppCompatActivity {
                 next();
             }
         });
+        spinner=(Spinner)findViewById(R.id.spinner71);
+        spinner.setOnItemSelectedListener(this);
+        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this,R.array.specialization , android.R.layout.simple_spinner_item );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
     private void previous()
     {
@@ -39,5 +49,16 @@ public class Activity7 extends AppCompatActivity {
     {
         Intent i=new Intent(this, Activity7_1.class);
         startActivity(i);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String s=parent.getItemAtPosition(position).toString();
+        Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
